e9cc5c954f20fda1f6a7ec0ecf650b1a3c1e00c3
Richard <>
2021-03-10 10:49:46 -0500

    got some code for selecting items in the list, need to work on deselecting tho

--- 441-Lists/441-Lists/ViewController.swift
+++ 441-Lists/441-Lists/ViewController.swift
@@ -10,7 +10,11 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var table: UITableView!
+    @IBOutlet weak var deleteButton: UIButton!
     
+    
+    var selectedCells: [UITableViewCell] = []
+    var selectedIndexes: [IndexPath] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,22 +23,47 @@ class ViewController: UIViewController {
         table.dataSource = self
         table.delegate = self
         
+        selectedCells = []
+        
+    }
+    
+    @IBAction func deleteRows(){
+//        table.deleteRows(at: selectedIndexes!, with: UITableView.RowAnimation(rawValue: 1)!)
     }
 }
 
+
 extension ViewController: UITableViewDelegate{
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath){
-        
+        let cell = tableView.cellForRow(at: indexPath)
+//        print(indexPath)
+        print(cell?.isHighlighted)
+        if(cell?.isHighlighted == true){
+            print("here")
+            cell?.isHighlighted = false
+            selectedCells = selectedCells.filter{$0 == cell}
+        }
+        else{
+            cell?.isHighlighted = true
+            selectedCells.append(cell!)
+            print(selectedCells.count)
+        }
+//        print(cell == selectedCells![0])
+
     }
 }
 
 extension ViewController: UITableViewDataSource{
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        <#code#>
+        return 5
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for:indexPath)
+        
+        cell.textLabel?.text = "hello World"
+        
+        return cell
     }
     
     
