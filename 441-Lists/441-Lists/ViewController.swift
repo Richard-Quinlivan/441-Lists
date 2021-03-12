ade342c69b935cd2cd0d3d01562fffdcd4d80d45
Richard <>
2021-03-12 13:14:34 -0500

    got it so you can now highlight and delete multiple cells

--- 441-Lists/441-Lists/ViewController.swift
+++ 441-Lists/441-Lists/ViewController.swift
@@ -12,60 +12,63 @@ class ViewController: UIViewController {
     @IBOutlet weak var table: UITableView!
     @IBOutlet weak var deleteButton: UIButton!
     
-    
-    var selectedCells: [UITableViewCell] = []
-    var selectedIndexes: [IndexPath] = []
-    
+    var items = ["Test1", "Test2", "Test3", "Test4", "Test5", "Test6"]
+        
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        table.dataSource = self
-        table.delegate = self
         
-        selectedCells = []
+        table.delegate = self
+        table.dataSource = self
+        
         
     }
     
     @IBAction func deleteRows(){
-//        table.deleteRows(at: selectedIndexes!, with: UITableView.RowAnimation(rawValue: 1)!)
+        let selectedIndexes:[IndexPath] = table.indexPathsForSelectedRows ?? []
+//        print(selectedIndexes)
+        var deleteRow = 0
+        if(selectedIndexes.count > 0){
+            var i = 0
+            while i < items.count{
+                print(items[i], table.cellForRow(at: selectedIndexes[deleteRow])?.textLabel!.text)
+                if(items[i] == table.cellForRow(at: selectedIndexes[deleteRow])?.textLabel!.text){
+                    items = items.filter{$0 != items[i]}
+                    i -= 1
+                    deleteRow += 1
+                    if(deleteRow >= selectedIndexes.count) {
+                        break
+                    }
+                }
+                i += 1
+            }
+            table.deleteRows(at: selectedIndexes, with: .right)
+
+        }
     }
 }
 
 
 extension ViewController: UITableViewDelegate{
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath){
-        let cell = tableView.cellForRow(at: indexPath)
-//        print(indexPath)
-        print(cell?.isHighlighted)
-        if(cell?.isHighlighted == true){
-            print("here")
-            cell?.isHighlighted = false
-            selectedCells = selectedCells.filter{$0 == cell}
-        }
-        else{
-            cell?.isHighlighted = true
-            selectedCells.append(cell!)
-            print(selectedCells.count)
-        }
-//        print(cell == selectedCells![0])
-
+        
     }
 }
 
 extension ViewController: UITableViewDataSource{
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return items.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for:indexPath)
         
-        cell.textLabel?.text = "hello World"
-        
+        cell.textLabel?.text = items[indexPath.row]
         return cell
     }
-    
-    
 }
 
